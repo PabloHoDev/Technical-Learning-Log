854a0bfc83e43af84593f6c8f89148964dc1e388
Pablo <pbl.oliveira.dev@gmail.com>
2026-04-22 16:13:34 -0300

    Update test_value_objects.py

--- 2026/projeto_dr_stone/reino_da_ciencia/tests/test_value_objects.py
+++
@@ -1,8 +0,0 @@
-from src.dominio.value_objects.tipo_recurso import TipoRecurso
-
-
-def test_tipo_recurso_enum():
-    assert TipoRecurso.MADEIRA.value == "madeira"
-    assert TipoRecurso.PEDRA.value == "pedra"
-    assert TipoRecurso.FERRO.value == "ferro"
-    assert TipoRecurso.ALIMENTO.value == "alimento"
